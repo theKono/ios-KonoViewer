047ffbbb2ea2ba50378203c9c4a0c6df5e79d1e8
mingshing <mingshing.kuo@thekono.com>
2017-07-18 10:34:29 +0800

    Initial Commit

---
+++ KonoViewerKit/KonoViewerKit.h
@@ -0,0 +1,13 @@
+//
+//  KonoViewerKit.h
+//  KonoViewerKit
+//
+//  Created by kuokuo on 2017/7/18.
+//  Copyright © 2017年 Kono. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KonoViewerKit : NSObject
+
+@end
---
+++ KonoViewerKit/KonoViewerKit.m
@@ -0,0 +1,13 @@
+//
+//  KonoViewerKit.m
+//  KonoViewerKit
+//
+//  Created by kuokuo on 2017/7/18.
+//  Copyright © 2017年 Kono. All rights reserved.
+//
+
+#import "KonoViewerKit.h"
+
+@implementation KonoViewerKit
+
+@end
