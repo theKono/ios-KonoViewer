76d82f1ce379aa311aba1a6be60f989b97c9ea9d
mingshing <mingshing.kuo@thekono.com>
2017-09-01 17:44:53 +0800

    [Feature] Implement fit-reading loading functionality
    - Loading the NSData format content
    - Implement the delegate to present user action

--- KonoViewerKit/KonoFitreadingView.h
+++ KonoViewerKit/KonoFitreadingView.h
@@ -5,9 +5,28 @@
 //  Created by kuokuo on 2017/8/14.
 //  Copyright © 2017年 Kono. All rights reserved.
 //
-
+#import <KonoContentKit/KonoContentKit.h>
 #import <UIKit/UIKit.h>
 
-@interface KonoFitreadingView : UIWebView
+@protocol KonoFitreadingViewDelegate <NSObject>
+
+- (void)userDidClickOnContent;
+
+@optional
+
+@end
+
+
+
+@interface KonoFitreadingView : UIWebView <UIWebViewDelegate>
+
+@property (nonatomic, weak) id<KonoFitreadingViewDelegate> actionDelegate;
+
+@property (nonatomic, strong) KCBook *bookItem;
+
+@property (nonatomic, strong) KCBookArticle *articleItem;
+
+
+- (void)renderFitreadingArticleFromData:(NSData*)data withImageRefPath:(NSString*)imagePath requireKey:(BOOL)requireKey;
 
 @end
