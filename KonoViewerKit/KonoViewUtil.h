bd5ad84076fdece2558c50bee77063f2683b4227
mingshing <mingshing.kuo@thekono.com>
2017-10-23 19:22:00 +0800

    [Feature] Generate HTML file from json data
      In order to support easier customization on fit-reading view,
    we parse the article json data into a well-structured HTML file.
      The vendor can replace their own css file for their customize
    view, also they can edit the HTML string to create additional
    viewer function support.
    
    - Use KonoViewUtil class as the viewer utility class
    - Add sample css json file for testing

---
+++ KonoViewerKit/KonoViewUtil.h
@@ -0,0 +1,20 @@
+//
+//  KonoViewUtil.h
+//  KonoViewerKit
+//
+//  Created by kuokuo on 2017/10/23.
+//  Copyright © 2017年 Kono. All rights reserved.
+//
+#import <KonoContentKit/KonoContentKit.h>
+#import <Foundation/Foundation.h>
+
+@interface KonoViewUtil : NSObject
+
++ (NSBundle *)viewerBundle;
+
++ (void)loadSampleJson;
+
++ (NSString *)getHTMLTemplateFromArticleDic:(NSDictionary *)articleDic withCSSFilePath:(NSString *)cssFilePath;
+
+
+@end
