6cc8e6e33c0e2c21dc0fced030343b4961759e4f
mingshing <mingshing.kuo@thekono.com>
2017-11-14 17:10:03 +0800

    [Feature] Add javascript for auto-scroll functionality
        Assume the ALC app will get the current index id through
    audio interface, we provide the javascript interface to
    highlight certain span area and scroll this area to top/middle
    of viewport.
    
    - highlightSentence
    - scrollSentenceToTop
    - scrollSentenceToMiddle

--- KonoViewerKit/Resource/main.js
+++ KonoViewerKit/Resource/main.js
@@ -1,4 +1,39 @@
-$( document ).ready(function(){
-    alert("test");
-                    
-});
+function highlightSentence(sentenceID) {
+    var objID = "#sentence-" + sentenceID;
+    var highlightObj = $(objID);
+    removeHighLight();
+    highlightObj.addClass('highlightBlock');
+}
+
+function removeHighLight() {
+    
+    $('.highlightBlock').removeClass('highlightBlock');
+    
+}
+
+function scrollSentenceToTop(sentenceID) {
+    
+    var objID = "#sentence-" + sentenceID;
+    var targetObj = $(objID);
+    
+    $(window).scrollTop(targetObj.offset().top);
+}
+
+function scrollSentenceToMiddle(sentenceID) {
+    
+    var objID = "#sentence-" + sentenceID;
+    var targetObj = $(objID);
+    var elementOffset = targetObj.offset().top;
+    var targetLocation = $(window).height()/2;
+    var moveOffset;
+    
+    if (elementOffset > targetLocation) {
+        moveOffset = elementOffset - targetLocation;
+    }
+    else {
+        moveOffset = elementOffset;
+    }
+    //$(window).scrollTop(moveOffset);
+    var speed = 800;
+    $('html, body').animate({scrollTop:moveOffset}, speed);
+}
